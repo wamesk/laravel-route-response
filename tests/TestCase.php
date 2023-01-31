66246d6059302b79b6111d6978ea75b63971323c
adrianzofcin <adrianzofcin@gmail.com>
2023-01-31 13:46:09 +0100

    Removed unused tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,8 +15,8 @@ class TestCase extends BaseTestCase
 
     public function setUp() : void
     {
-        Log::channel('stderr')->info('Test set up in progress');
         parent::setUp();
+        Log::channel('stderr')->info('Test set up in progress');
 
         ini_set('memory_limit', '512M');
         ini_set('max_execution_time', '600');
@@ -29,11 +29,9 @@ class TestCase extends BaseTestCase
         if (isset($userData['password'])) $userData['password_confirmation'] = $userData['password'];
 
         $routePath = route('auth.register');
-        Log::channel('stderr')->info('ROUTE' . $routePath);
         $response = $this->postJson($routePath, $userData);
 
         $response->assertStatus(201);
-        Log::channel('stderr')->info('SUCCESS ' . $routePath);
 
         Config::set('wame-route.testing', [
             'user' => $response['data']['user'],
